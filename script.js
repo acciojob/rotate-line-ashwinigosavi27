1895d1020982c7d7f703d5db560a27944dadb62f
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-06-28 21:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+//your JS code here. If required.
+var line = document.getElementById("line");
+var angle = 0;
+var rotationSpeed = 2;
+function rotateLine(){
+	angle += rotationSpeed;
+	line.style.transform = "rotate(" + angle + "deg)";
+
+  // Request the next frame to continue the rotation
+  requestAnimationFrame(rotateLine);
+	
+	
+}
+rotateLine();
